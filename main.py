dfc7c4da4db3cd2bab6ae4351484f4d52db1fee4
Rotar Alex <rotaralexc@gmail.com>
2023-03-12 20:54:46 +0200

    Create columns, image and description

---
+++ main.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Rotar Alex")
+    content = """"
+    Hello, my name is Alex!
+    University fresh graduated and for the moment working as an System Test Engineer at NTT DATA Romania.
+    I am extremply motivated to learn new thing and to apply them in the real world as projects.
+    """
+    st.info(content)
